34b187a8e222c665009085a107af894bbe726ee6
Aryan <107269876+pikachu71213@users.noreply.github.com>
2026-06-03 16:48:44 +0530

    fix: update base path in Vite configuration to correct project structure

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/langma-internaional-main/',
+  base: '/langma',
 
   build: {
     outDir: 'dist',
